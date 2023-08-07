47bee2ecc343ef049ae2592b0527d4cfc763b656
Massiels <114120172+Massiels@users.noreply.github.com>
2023-08-07 16:23:07 -0500

    Update index.js
    Remove code hours

--- index.js
+++ index.js
@@ -1,33 +1,4 @@
-//date
-let now = new Date();
 
-let h6 = document.querySelector("h6");
-
-let date = now.getDate();
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let year = now.getFullYear();
-
-let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-let day = days[now.getDay()];
-
-let months = [
-  "Jan",
-  "Feb",
-  "March",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec"
-];
-let month = months[now.getMonth()];
-
-h6.innerHTML = `${day} ${month} ${date}, ${hours}:${minutes}, ${year}`;
 
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
